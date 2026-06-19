8ac96ba5ae9bb3dd6f025dbde75382ad8859b60a
leonardovarona42 <leonardovarona42@gmail.com>
2026-06-19 12:31:52 -0700

    fix: redirigir / a login si no autenticado

--- Inventory_Sales/urls.py
+++ Inventory_Sales/urls.py
@@ -8,23 +8,24 @@ from django.conf.urls.static import static
 from django.views.static import serve as static_serve
 from django.db.models import Sum, F
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from ventas.models import Venta
 from productos.models import Producto
 
 
 def home_view(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
     context = {}
-    if request.user.is_authenticated:
-        hoy = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
-        ventas_hoy = Venta.objects.filter(fecha_venta__gte=hoy).count()
-        total_hoy = Venta.objects.filter(fecha_venta__gte=hoy).aggregate(Sum('total_pagado'))['total_pagado__sum'] or 0
-        productos_activos = Producto.objects.filter(stock_actual__gt=0).count()
-        stock_bajo = Producto.objects.filter(stock_actual__lt=F('stock_minimo')).count()
-        context['ventas_hoy'] = ventas_hoy
-        context['total_hoy'] = total_hoy
-        context['productos_activos'] = productos_activos
-        context['stock_bajo'] = stock_bajo
+    hoy = timezone.now().replace(hour=0, minute=0, second=0, microsecond=0)
+    ventas_hoy = Venta.objects.filter(fecha_venta__gte=hoy).count()
+    total_hoy = Venta.objects.filter(fecha_venta__gte=hoy).aggregate(Sum('total_pagado'))['total_pagado__sum'] or 0
+    productos_activos = Producto.objects.filter(stock_actual__gt=0).count()
+    stock_bajo = Producto.objects.filter(stock_actual__lt=F('stock_minimo')).count()
+    context['ventas_hoy'] = ventas_hoy
+    context['total_hoy'] = total_hoy
+    context['productos_activos'] = productos_activos
+    context['stock_bajo'] = stock_bajo
     return render(request, 'home.html', context)
 
 
